7dc3f73d38e5e3af7b183688fe3e0a658db2401f
igaue-takahhiko <hasewo.2610@gmail.com>
2020-11-04 17:00:40 +0900

    setting router pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import Login from './Login';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Route exact path="/" component={App} />
+            <Route exact path="/login" component={Login} />
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
