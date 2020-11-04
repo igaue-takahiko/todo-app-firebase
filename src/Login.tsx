0a5c992e4bc3f86b2c76552828c0d4765f432d16
igaue-takahhiko <hasewo.2610@gmail.com>
2020-11-04 18:09:35 +0900

    Implement Login component

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, FormControl, TextField, Typography } from '@material-ui/core';
 
-const Login: React.FC = () => {
+import styles from './Login.module.css';
+import { auth } from "./firebase/config";
+
+const Login: React.FC = (props: any) => {
+    const [ isLogin, setIsLogin ] = useState(true)
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
+
+    useEffect(() => {
+        const unSub = auth.onAuthStateChanged(user => {
+            user && props.history.push("/")
+        })
+        return () => unSub()
+    },[props.history])
+
     return (
-        <div>
-            
+        <div className={styles.login__root}>
+            <h1>{isLogin ? "Login" : "Register"}</h1>
+            <br/>
+            <FormControl>
+                <TextField
+                    name="email" label="Email address" value={email}
+                    InputLabelProps={{ shrink: true }}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setEmail(e.target.value)
+                    }}
+                />
+            </FormControl>
+            <br/>
+            <FormControl>
+                <TextField
+                    name="password" label="Password" type="password"
+                    value={password} InputLabelProps={{ shrink: true }}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setPassword(e.target.value)
+                    }}
+                />
+            </FormControl>
+            <br/>
+            <Button
+                variant="contained" color="primary" size="small"
+                onClick={
+                    isLogin
+                        ? async () => {
+                            try {
+                                await auth.signInWithEmailAndPassword(email, password)
+                                props.history.push("/")
+                            } catch (error) {
+                                alert(error.message)
+                            }
+                        }
+                        : async () => {
+                            try {
+                                await auth.createUserWithEmailAndPassword(email, password)
+                                props.history.push("/")
+                            } catch (error) {
+                                alert(error.message)
+                            }
+                        }
+                }
+            >
+                {isLogin ? "login" : "register"}
+            </Button>
+            <br/>
+            <Typography align="center">
+                <span onClick={() => setIsLogin(!isLogin)}>
+                    {isLogin ? "Create new account ?" : "Back to login"}
+                </span>
+            </Typography>
         </div>
     )
 }
